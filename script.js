c05eb4a3beaad13ddc9e8ea59f3be11f1b26cc86
joshdelmonte <joshdelmonte@gmail.com>
2022-08-29 11:08:51 -0600

    Added a few lines of code to geratePassword

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword(){
+    console.log("Hello! Would you like to generate a new password?")
+
+    return "<GENERATED PASSWORD>"
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
+  
   passwordText.value = password;
 
 }
