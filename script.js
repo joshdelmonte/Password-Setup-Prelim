6d375adf4db21cef65b2ece6d6771e4ff3a41814
joshdelmonte <joshdelmonte@gmail.com>
2022-08-31 12:13:44 -0600

    Trying to prompt user for yes or no questions

--- script.js
+++ script.js
@@ -8,20 +8,24 @@ function generatePassword(){
     var starterQuery = window.confirm("Would you like us to generate a  unique password? ");
     console.log(starterQuery);    
 // prompt user for password length
-    var numberInput = window.prompt("How many characters would you like your password to be?");
+    var userInput = window.prompt("How many characters would you like your password to be?");
 // set perameters for password length
-    var passwordLength = parseInt(numberInput);
+    var passwordLength = parseInt(userInput);
 
     if (passwordLength < 8 || passwordLength > 128) {
     window.alert("Password length must be between 8-128 characters.");
-    return
+    return;
     }
-    // added after realizing people will be people  
-    if (isNaN(passwordLength) {
-     window.alert("Invalid input. Please enter a numerical value between 8-128.")
-    // WHY ISN'T this return working?
-     return
+// added after realizing people will be people
+    if (isNaN(passwordLength)) {
+    window.alert("Invalid input. Please enter a numerical value between 8-128.");
+    return;
     }
+
+    var wantedNumbers = window.confirm("Would you like numbers in your new password?");
+    var wantedSymbols = window.confirm("Care for any unique characters in your new password?");
+    var wantedLowLetters = window.confirm("Would you like lower case letters in your new password?");
+    var wantedUpperChars = window.confirm("Would you like to add upper cased letters to further enhance your new password?");
 
 }
 
